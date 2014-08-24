28cf4b559d7a9c061000838b328ad397f1a65856
Jack <jackcrawfo@gmail.com>
2014-08-23 23:41:43 -0400

    started working on algo

--- algo/algoMain.py
+++ algo/algoMain.py
@@ -1,11 +1,19 @@
+from apiclient.discovery import build
+from oauth2client.client import OAuth2WebServerFlow
+
+import httplib2
+
 NUM_PATIENTS = 0
 NUM_CARE_TEAMS = 0
 NUM_SLOTS = 96
+INTERVAL_MINUTES = 15
+INTERVAL_MS = INTERVAL_MINUTES*60*1000
 
 PatientArray = np.zeros((NUM_PATIENTS, NUM_SLOTS))
-CareTeamArray = np.zeros((NUM_CARE_TEAMS, NUM_SLOTS))
+CareTeamArray = np.zeros((NUM_CARE_TEAMS, NUM_SLOTS))n 
+FlexTimes = {}
 
-
+AllOrders = []
 
 import numpy as np
 
@@ -29,8 +37,10 @@ class WorkOrder:
 
 
 def main():
-    
-
-    
-    
-        
+    for workOrder in AllOrders:
+        for i in range(workOrder.expectedStart, workOrder.expectedEnd+1, INTERVAL_MINUTES)
+            if not PatientArray[pNum][i] == 0 or not PatientArray[ctNum][i] == 0:
+                flag = true
+                break
+            
+                
